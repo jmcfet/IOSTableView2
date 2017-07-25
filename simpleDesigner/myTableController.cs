4aeafedf824d49eca4759982bf3196b8d241d3d4
john mcfetridge <jmcfet@bellsouth.net>
2017-07-25 15:25:58 -0400

    Add project files.

---
+++ simpleDesigner/myTableController.cs
@@ -0,0 +1,24 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace simpleDesigner
+{
+    public partial class myTableController : UITableViewController
+    {
+        public myTableController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+             this.NavigationItem.Title = "Vegs";
+            string[] tableItems = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
+            TableView.Source = new TableSource(tableItems);
+          
+        }
+    }
+    
+}
